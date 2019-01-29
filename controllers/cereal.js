460fce265145de416441594e3710c5ad78c35566
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-29 11:04:58 -0500

    create function complete

--- controllers/cereal.js
+++ controllers/cereal.js
@@ -1,6 +1,18 @@
 const Cereal = require('../models/Cereal')
 
 module.exports = {
+    create: (req, res) => {
+        Cereal.create({
+            name: req.body.cereal.name,
+            brand: req.body.cereal.brand,
+            picture: req.body.cereal.picture,
+            milkType: req.body.cereal.milkType,
+            review: req.body.cereal.review
+        })
+        .then(cereal => {
+            res.redirect(`cereal/${cereal._id}`)
+        })
+    },
     new: (req, res) => {
         res.render('cereal/new')
     },
@@ -9,6 +21,12 @@ module.exports = {
         .then(cereal => {
             console.log(cereal)
             res.render("cereal/show", { cereal })
+        })
+    },
+    delete: (req, res) => {
+        Cereal.findOneAndRemove({ _id: req.params.id })
+        .then( () => {
+            res.redirect('/')
         })
     }
 }
--- routes/cereal.js
+++ routes/cereal.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const cerealController = require('../controllers/cereal')
 
+router.post('/', cerealController.create)
 router.get('/new', cerealController.new)
 router.get('/:id', cerealController.show)
+router.delete('/:id', cerealController.delete)
 
 module.exports = router
