b5d8016324a380fa011e7388218cc8695ccba689
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-29 10:13:36 -0500

    made form for create function

--- controllers/cereal.js
+++ controllers/cereal.js
@@ -1,6 +1,9 @@
 const Cereal = require('../models/Cereal')
 
 module.exports = {
+    new: (req, res) => {
+        res.render('cereal/new')
+    },
     show: (req, res) => {
         Cereal.findOne({ _id: req.params.id })
         .then(cereal => {
--- routes/cereal.js
+++ routes/cereal.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const cerealController = require('../controllers/cereal')
 
-// router.get('/new', cerealController.new)
+router.get('/new', cerealController.new)
 router.get('/:id', cerealController.show)
 
 module.exports = router
