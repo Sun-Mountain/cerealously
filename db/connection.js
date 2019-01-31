5389da63904606c0476b2469485bd381c25ec764
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-31 11:19:46 -0500

    mlab_url added to connection

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV == "production") {
-    mongoose.connect('process.env.cerealously')
+    mongoose.connect(process.env.MLAB_URL)
   } else {
     mongoose.connect('mongodb://localhost/cerealously');
   }
