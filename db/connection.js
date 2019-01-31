f2a93c1479485af9368ae898787dce0be21f0199
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-31 09:57:31 -0500

    heroku app deploy attempt 1

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/cereal')
 mongoose.Promise = Promise
+
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.cerealously)
+  } else {
+    mongoose.connect("mongodb://localhost/cerealously");
+  }
+
 module.exports = mongoose
