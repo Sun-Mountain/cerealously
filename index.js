7cef497fc60b26c7a2c7a2ceefa27d71e4f461e9
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-31 11:41:19 -0500

    fixing index.js maybe

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.static("public"))
 
 app.use(require("./routes/index.js"))
 
-app.set('port', process.env.PORT || 3001)
+app.set('port', process.env.PORT || 3000)
 
-app.listen((app.get('port'), () => {
-    console.log(`✅ PORT: ${app.get('port')} 🌟`)
-}))
+app.listen(app.get('port'), () => console.log(`server is running on PORT ${app.get('port')}`));
