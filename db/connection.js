6c95f03383c117dd3c94f869a78d67238a72ec3c
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-31 11:12:33 -0500

    it's yelling at me for no reason now

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV == "production") {
     mongoose.connect(process.env.cerealously)
   } else {
-    mongoose.connect("mongodb://localhost/cerealously");
+    mongoose.connect('mongodb://localhost/cerealously');
   }
 
 mongoose.Promise = Promise
