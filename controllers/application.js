9136d60776f5d8711387bf1d7ef8daae83592199
Sun-Mountain <npzonnenberg@gmail.com>
2019-02-01 10:59:21 -0500

    coding is fun part 3

--- controllers/application.js
+++ controllers/application.js
@@ -9,7 +9,7 @@ module.exports = {
             .then(cereals => {
                 News.find({})
                 .sort({ createdAt: -1 })
-                .limit(3)
+                .limit(1)
                 .then(news => {
                     res.render("app/index", { cereals, news })
             })
